09254bed53e855fa4013259c1b2dc770e43757fb
xbeatzsec <75812403+xbeatzsec@users.noreply.github.com>
2021-02-13 15:11:49 +0000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,48 +2,7 @@
 !(function ($) {
   "use strict";
 
-  // Desafio para o professor :)!!
-  $(document).keydown(function (event) {
-    if (event.keyCode == 123) { //evitar  F12
-      document.querySelector('#hero').scrollIntoView({
-        behavior: 'smooth'
-      });
-      document.getElementById("h1_p").innerHTML = "F12? Really?"
-      return false;
-    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // evitar Ctrl+Shift+I
-      document.querySelector('#hero').scrollIntoView({
-        behavior: 'smooth'
-      });
-      document.getElementById("h1_p").innerHTML = "Ohhhhhhhh you really close!"
 
-      return false;
-    }
-    document.addEventListener('contextmenu', event => event.preventDefault()); //Evitar o clike do lado direito
-  });
-  // ctrl + u evitado
-  document.onkeydown = function (e) {
-    if (e.ctrlKey &&
-      (e.keyCode === 67 ||
-        e.keyCode === 86 ||
-        e.keyCode === 85 ||
-        e.keyCode === 117)) {
-      document.querySelector('#hero').scrollIntoView({
-        behavior: 'smooth'
-      });
-      document.getElementById("h1_p").innerHTML = "Someone is trying to view the source code :)"
-      return false;
-    } else {
-      return true;
-    }
-  };
-  $(document).keypress("u", function (e) {
-    if (e.ctrlKey) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  });
 
   // smooth scroll
   var scrolltoOffset = $('#header').outerHeight() - 17;
